e21bd77b7e6d81eea315ae37ca9599f9c669cb3c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-21 16:33:43 +0500

    Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,30 @@
-import type { Metadata } from 'next'
-import { Work_Sans } from 'next/font/google'
-import './globals.css'
-import { Room } from './Room'
+import { Work_Sans } from "next/font/google";
 
-const inter = Work_Sans({
-  subsets: ['latin'],
-  variable: '--font-work-sans',
-  weight: ['400', '600', '700']
-})
+import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-export const metadata: Metadata = {
-  title: 'Design Pro | Turk',
+import Room from "./Room";
+
+export const metadata = {
+  title: "Figma Clone",
   description:
-    'Design Pro using Fabrics.js && Liveblocks for realtime collaboration'
-}
+    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
+};
 
-export default function RootLayout ({
-  children
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang='en'>
-      <body className={inter.className + ' !bg-primary-grey-200'}>
-        <Room>{children}</Room>
-      </body>
-    </html>
-  )
-}
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["400", "600", "700"],
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang='en'>
+    <body className={`${workSans.className} bg-primary-grey-200`}>
+      <Room>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Room>
+    </body>
+  </html>
+);
+
+export default RootLayout;
